955cdd454327d87187c3e0f7291b89b31e16829f
kauerds01 <rodrigueskaue087@gmail.com>
2026-04-29 14:10:20 +0000

    v1

--- sentilnela.py
+++ sentilnela.py
@@ -1,1 +1,6 @@
-
+#--Váriavel Sentinela--
+while True:
+    comando = input("Digite o Comando, se precisar parar digite: sair")
+    if comando == "sair":
+        break
+    print(f"executando:{comando}")
