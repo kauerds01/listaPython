a82dea4f2bfc2f8b41aac05683e17d42811862db
kauerds01 <rodrigueskaue087@gmail.com>
2026-05-04 07:47:37 -0300

    Update seletor.py

--- seletor.py
+++ seletor.py
@@ -1,1 +1,8 @@
-
+#Seletor de Caracteres--
+letra = input("Digite uma letra:").upper()
+if letra == 'A':
+  print("Você Selecionou a Opção A")
+elif letra == 'B':
+  print("Você Selecionou a Opção B")
+else:
+  print("Essa Opção Não está disponível")
