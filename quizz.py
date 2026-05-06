b39218a9a655457b804dfca91d18339fd023e40a
kauerds01 <rodrigueskaue087@gmail.com>
2026-05-06 14:29:12 +0000

    V1

--- quizz.py
+++ quizz.py
@@ -1,1 +1,10 @@
-
+#--Quizz--
+print("Bem vindo ao Quizz Donos da Bola😁")
+print("Nesse Quiz iremos lhe fazer perguntas sobre a Copa do Mundo de 2026⚽")
+print("Responda somente a Letra da alternativa que julgar correta🔠")
+questão1 = input("1-Quais Países serão palco da Copa do Mundo de 2026? \n A-Inglaterra, Espanha, França \n B-Canadá, Estados Unidos e México \n C-Brasil, Argentina e Uruguai").upper()
+if questão1 == "B":
+    print("Resposta Correta✅")
+else:
+    print("Resposta Incorreta❌")
+questão2 = input("")
