b49a441d63a001e1170fe23a9c9914fb4648a7ff
kauerds01 <rodrigueskaue087@gmail.com>
2026-05-18 11:12:44 +0000

    V1

--- tabuada.py
+++ tabuada.py
@@ -1,1 +1,6 @@
-
+print("Bem vindo a sua Tabuada Digital😁")
+numero = int(input("Qual o número da tabuada que você deseja? "))
+print(f"---Tabuda do {numero}---")
+for i in range (1,11):
+    resultado = numero * i
+    print(f"{numero} x {i} = {resultado}")
