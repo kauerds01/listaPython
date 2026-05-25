69740da171ec1f17c52527a5043d0213d91d05d5
kauerds01 <rodrigueskaue087@gmail.com>
2026-05-25 23:20:01 +0000

    V2

--- simulador_investimento_cdb.py
+++ simulador_investimento_cdb.py
@@ -1,1 +1,10 @@
 #Simulador CDB 🪙--
+aporte = float(input("Quanto você irá investir por mês?"))
+meses = int(input("Quantos meses você irá investir?"))
+juros = 1.24/100
+total = 0
+for mes in range(1, meses +1):
+    total = total + aporte
+    total = total + (total*juros)
+    print(f"Mês{mes}:Saldo total= R${total}")
+print(f"Ao final de {meses} meses, você terá um valor de {total:.2f}")
