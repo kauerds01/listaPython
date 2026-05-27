bd8ec9741211fc59a63209e5b0116941de2a0477
kauerds01 <rodrigueskaue087@gmail.com>
2026-05-27 13:15:49 +0000

    V1

---
+++ cabecalho.py
@@ -0,0 +1,10 @@
+# Definindo e Chamando a função--
+def exibir_cabecalho():
+    print("=" * 30)
+    print("SISTEMA DE GESTÃO ESCOLAR")
+    print("Versão 1.0 - Desenvolvimento no Codespaces")
+    print("=" * 30)
+
+#Chamando a função
+exibir_cabecalho()
+print("Menu Principal:")
