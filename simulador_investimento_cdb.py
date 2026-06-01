5c0a2af2b0ff16e7986aaf08f835ff6d0c631492
kauerds01 <rodrigueskaue087@gmail.com>
2026-05-31 21:21:33 -0300

    Update simulador_investimento_cdb.py

--- simulador_investimento_cdb.py
+++ simulador_investimento_cdb.py
@@ -10,3 +10,4 @@ for mes in range(1, meses +1):
     total = total + (total*juros)
     print(f"Mês{mes}:Saldo total= R${total}")
 print(f"Ao final de {meses} meses, você terá um valor de {total:.2f}")
+#Kauê Rodrigues da Silva
